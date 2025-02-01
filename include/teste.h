441ea6dd6025e6218e9829c4d96e33b1a063e857
srspratt <hrodeberthus@gmail.com>
2025-02-01 15:17:26 -0300

    Adicionado o sufixo na bibliotexa; Atualizações nos CMakeLists

--- include/teste.h
+++
@@ -1,7 +0,0 @@
-#ifndef TESTE_H
-#define TESTE_H
-
-#include <stdio.h>
-#define HELLO printf("HELLO")
-
-#endif
--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
-#include <teste.h>
+#include <MainLED_U4C.h>
 
 int main(){
     const uint led_pin = 13;
 
     stdio_init_all();
-    teste();
+    Teste();
 
 }
